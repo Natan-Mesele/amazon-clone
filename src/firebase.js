a35c7f4b2a79aa00e4b2843aa761e968954d40a4
Natan-Mesele <huluale12@gmail.com>
2023-10-03 10:06:09 +0300

    f

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-import firebase from 'firebase/app'
+import firebase from 'firebase/compat/app'
+import 'firebase/firestore'
 
 const firebaseConfig = firebase.initializeApp ({
     apiKey: "AIzaSyDnnKbHDDJuKCge8Ym5m-38bobmQLPRhSs",
