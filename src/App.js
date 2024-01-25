18670167bfade8a986697998c7e7eedeb7b60af0
Natan-Mesele <huluale12@gmail.com>
2024-01-25 12:15:46 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
-import Cart from './components/Cart';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React from 'react'
+import Header from './components/Header'
 
 function App() {
   return (
-    <Router>
-    <div className="App">
-      <Header/>
-      <Switch>
-          <Route path="/Cart">
-            <Cart />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+    <div>
+      <Header />
     </div>
-    </Router>
-  );
+  )
 }
 
-export default App;
+export default App
